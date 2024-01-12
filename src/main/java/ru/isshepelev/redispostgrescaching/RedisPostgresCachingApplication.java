860eab9783445ca7b2a8d40f98beaea5051035ca
ilya <isshepelev2004@gmail.com>
2024-01-12 20:45:48 +0300

    first commit

---
+++ src/main/java/ru/isshepelev/redispostgrescaching/RedisPostgresCachingApplication.java
@@ -0,0 +1,13 @@
+package ru.isshepelev.redispostgrescaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisPostgresCachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisPostgresCachingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/isshepelev/redispostgrescaching/RedisPostgresCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.isshepelev.redispostgrescaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisPostgresCachingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
